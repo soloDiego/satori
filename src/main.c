fd288900562b0a331ecbf4cbc98666abcf0490aa
soloDiego <solo.dev.diego@proton.me>
2026-05-01 22:41:04 -0700

    feat: get registry, attach listeners, log globals

--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <wayland-client.h>
+
+static void registry_global(void *data, struct wl_registry *registry,
+                            uint32_t name, const char *interface,
+                            uint32_t version) {
+    fprintf(stderr, "global: name:%u, interface:%s, version%u\n",
+            name, interface, version);
+
+    (void)data;
+    (void)registry;
+}
+
+static void registry_global_remove(void *data, struct wl_registry *registry,
+                                   uint32_t name) {
+    fprintf(stderr, "global remove: name:%u", name);
+
+    (void)data;
+    (void)registry;
+}
+
+static const struct wl_registry_listener registry_listener = {
+    .global = registry_global,
+    .global_remove = registry_global_remove,
+};
 
 int main(void) {
     struct wl_display *display = wl_display_connect(NULL);
@@ -9,7 +33,13 @@ int main(void) {
                 "(is WAYLAND_DISPLAY set and a compositor running?)\n");
         return 1;
     }
+
+    struct wl_registry *registry = wl_display_get_registry(display);
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+    wl_display_roundtrip(display);
+
     
+    wl_registry_destroy(registry);
     wl_display_disconnect(display);
     return 0;
 }
