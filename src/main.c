a5c8ae99d94849bc311b76158dc510369633791b
soloDiego <solo.dev.diego@proton.me>
2026-05-01 21:49:44 -0700

    feat: connect and disconnect from wayland display

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <wayland-client.h>
+
 int main(void) {
+    struct wl_display *display = wl_display_connect(NULL);
+
+    if (!display) {
+        fprintf(stderr, "could not connect to wayland display "
+                "(is WAYLAND_DISPLAY set and a compositor running?)\n");
+        return 1;
+    }
+    
+    wl_display_disconnect(display);
     return 0;
 }
